153948a7f9cd9606ad2f3bde7d4d78c4a75ed19b
hamedj <hamedjamali73@gmail.com>
2024-03-11 17:31:50 +0330

    add search

---
+++ src/component/search/index.js
@@ -0,0 +1,57 @@
+import React, {
+  memo,
+  useDeferredValue,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from "react";
+import { SearchIcon } from "../icon-svg";
+
+const Search = memo(({ data, searchList, dataFromServer }) => {
+  const [search, setSearch] = useState("");
+  const query = useDeferredValue(search);
+
+  const SearchFilter = (v) => {
+    let input = v?.target?.value ?? "";
+    setSearch(input);
+    dataFromServer(input)
+    // static handle
+    // if (input?.length > 0) {
+    //   let name = data?.filter((v) => v.first_name.toLowerCase().includes(input)) ?? [];
+    //   let phone = data?.filter((v) => v.phone.includes(input)) ?? [];
+    //   let city = data?.filter((v) => v.address?.toLowerCase().includes(input)) ?? [];
+    //   searchList([...name, ...phone, ...city]);
+    // } else {
+    //   searchList(data);
+    // }
+  };
+  return (
+    <div className="last-search sticky top-5">
+      <form className="max-w-md mx-auto">
+        <label
+          htmlFor="default-search"
+          className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
+        >
+          Search
+        </label>
+        <div className="relative">
+          <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+            <SearchIcon />
+          </div>
+          <input
+            type="search"
+            id="default-search"
+            className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="Search Name, Phone..."
+            onChange={SearchFilter}
+            value={search}
+            required
+          />
+        </div>
+      </form>
+    </div>
+  );
+});
+
+export default Search;
--- src/pages/contacts/details/index.js
+++ src/pages/contacts/details/index.js
@@ -1,28 +1,37 @@
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
 
 const ContactDetails = () => {
   const { id } = useParams();
-  const [contact, setContact] = useState(null);
+  const [contactDetail, setContactDetail] = useState(null);
 
   useEffect(() => {
-    axios.get(`/${id}`)
-      .then(response => setContact(response.data))
-      .catch(error => console.error(error));
-  }, [id]);
+    axios
+      .get(`http://localhost:1337/passenger/${id}`)
+      .then((response) => setContactDetail(response.data))
+      .catch((error) => console.error(error));
+  }, []);
 
   return (
-    <div>
-      {contact ? (
-        <div>
-          <h1>{contact.name}</h1>
-          {/* Display more details here */}
-        </div>
+    <section>
+      {contactDetail ? (
+        <>
+          {contactDetail.map((v, i) => {
+            return (
+              <div>
+                <div className="profile"></div>
+                <div className="sectionOne"></div>
+                <div className="sectionTwo"></div>
+                <div className="sectionThree"></div>
+              </div>
+            );
+          })}
+        </>
       ) : (
         <p>Loading...</p>
       )}
-    </div>
+    </section>
   );
 };
 
